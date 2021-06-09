3545df0dd2c1eb9b227a5a5a2718310606182d0e
ziulovatsug <gustavoluck12@gmail.com>
2021-06-09 12:56:56 -0300

    :)

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,15 +1,9 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
+import keys from "./keys"
 
-const config = {
-    apiKey: "AIzaSyBMdpE3S9k3FstIWdg6yOgpfr5_7cVbc9Y",
-    authDomain: "meus-projetos-6a53c.firebaseapp.com",
-    databaseURL: "https://meus-projetos-6a53c-default-rtdb.firebaseio.com",
-    projectId: "meus-projetos-6a53c",
-    storageBucket: "meus-projetos-6a53c.appspot.com",
-    messagingSenderId: "304317702121",
-    appId: "1:304317702121:web:f6496cdfc0b5092f663578"
-  };
+
+const config = keys;
 
  if (!firebase.apps.length) {
     firebase.initializeApp(config);
